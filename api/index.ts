47f925e014a58e4a5c36786d570d107ff4ba76f3
Hairum Qureshi <hqureshi@udel.edu>
2024-10-05 23:32:32 -0400

    setup MongoDB connection

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import colors from "colors";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 dotenv.config();
 colors.enable();
@@ -10,7 +11,7 @@ colors.enable();
 const app = express();
 
 const corsOptions = {
-	origin: "http://localhost:5173",
+	origin: process.env.FRONTEND_URL,
 	credentials: true,
 	optionSuccessStatus: 200
 };
@@ -22,6 +23,36 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT: string | number = process.env.PORT || 4000;
 
+const MONGO_URI: string = process.env.MONGO_URI!;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(MONGO_URI, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true
+	}
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!".green
+				.bold
+		);
+	} catch (error) {
+		console.log("<index.ts> error", (error as Error).toString().red.bold);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}!`);
+	console.log(`Server listening on port ${PORT}!`.yellow.bold);
+	run().catch(console.dir);
 });
