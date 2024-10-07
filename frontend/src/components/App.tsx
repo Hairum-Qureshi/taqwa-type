9431726e78a9c6f8d1a4b6d3326354039b0f966f
Hairum Qureshi <hqureshi@udel.edu>
2024-10-07 00:00:10 -0400

    added a Navbar component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Account from "./Account";
+import NotFound from "./NotFound";
+import Navbar from "./Navbar";
 
 export default function App() {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
-				{/* <Route path="*" element={<NotFound />} /> */}
+				<Route path="/account" element={<Account />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
