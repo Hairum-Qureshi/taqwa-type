8cf88a722f26e1c99970eb9dae9c8b4872f23f46
Hairum Qureshi <hqureshi@udel.edu>
2024-10-12 16:22:38 -0400

    implemented logic so users can switch between sign up and sign in form

--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -11,6 +11,7 @@ export default function AuthForm() {
 	const [lastName, setLastName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [showSignUp, setShowSignUp] = useState(true);
 
 	const { googleAuth, signUp } = useAuth();
 
@@ -33,60 +34,109 @@ export default function AuthForm() {
 		}
 	});
 
+	function update() {
+		setShowSignUp(!showSignUp);
+		setEmail("");
+		setPassword("");
+	}
+
 	return (
 		<div className="flex items-center justify-center flex-col mt-10">
 			<form className="flex items-center justify-center flex-col w-10/12">
 				<div className="mb-5 text-center">
-					<h2 className="text-3xl text-center text-white font-semibold">
-						Sign Up to Taqwa Type
-					</h2>
-					<p className="text-green-500 mt-3">
-						Kill two birds with one stone. Start today, it's free.
-					</p>
-					<p className="text-cyan-400 mt-2">
-						Don't have an account? No problem.&nbsp;
-						<Link to="/" className="underline">
-							Create one
-						</Link>
-						.
-					</p>
+					{showSignUp ? (
+						<>
+							<h2 className="text-3xl text-center text-white font-semibold">
+								Sign Up to Taqwa Type
+							</h2>
+							<p className="text-green-500 mt-3">
+								Kill two birds with one stone. Start today, it's free.
+							</p>
+							<p className="text-cyan-400 mt-2">
+								Already have an account?&nbsp;
+								<Link to="/" className="underline" onClick={update}>
+									Sign in
+								</Link>
+							</p>
+						</>
+					) : (
+						<>
+							<h2 className="text-3xl text-center text-white font-semibold">
+								Sign In to Taqwa Type
+							</h2>
+							<p className="text-green-500 mt-3">Welcome back to Taqwa Type!</p>
+							<p className="text-cyan-400 mt-2">
+								Don't have an account?&nbsp;
+								<Link to="/" className="underline" onClick={update}>
+									Create one
+								</Link>
+							</p>
+						</>
+					)}
 				</div>
-				<div className="flex flex-row w-full mb-5 items-center justify-center">
-					<input
-						type="text"
-						placeholder="First Name"
-						className="w-full mr-2 p-2 text-md outline-none rounded-sm"
-						value={firstName}
-						onChange={e => setFirstName(e.target.value)}
-					/>
-					<input
-						type="text"
-						placeholder="Last Name"
-						className="w-full ml-2 p-2 text-md outline-none rounded-sm"
-						value={lastName}
-						onChange={e => setLastName(e.target.value)}
-					/>
-				</div>
-				<input
-					type="email"
-					placeholder="Email"
-					className="w-full mb-5 mt-5 p-2 text-md outline-none rounded-sm"
-					value={email}
-					onChange={e => setEmail(e.target.value)}
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					className="w-full p-2 mt-5 text-md outline-none rounded-sm"
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-				/>
-				<button
-					className="bg-green-600 font-semibold text-white text-lg mt-10 py-2 p-2 w-full mb-10 rounded-sm"
-					onClick={e => signUp(e, firstName, lastName, email, password)}
-				>
-					Sign Up
-				</button>
+				{showSignUp ? (
+					<>
+						<div className="flex flex-row w-full mb-5 items-center justify-center">
+							<input
+								type="text"
+								placeholder="First Name"
+								className="w-full mr-2 p-2 text-md outline-none rounded-sm"
+								value={firstName}
+								onChange={e => setFirstName(e.target.value)}
+							/>
+							<input
+								type="text"
+								placeholder="Last Name"
+								className="w-full ml-2 p-2 text-md outline-none rounded-sm"
+								value={lastName}
+								onChange={e => setLastName(e.target.value)}
+							/>
+						</div>
+						<input
+							type="email"
+							placeholder="Email"
+							className="w-full mb-5 mt-5 p-2 text-md outline-none rounded-sm"
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+						/>
+						<input
+							type="password"
+							placeholder="Password"
+							className="w-full p-2 mt-5 text-md outline-none rounded-sm"
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+						/>
+						<button
+							className="bg-green-600 font-semibold text-white text-lg mt-10 py-2 p-2 w-full mb-10 rounded-sm"
+							onClick={e => signUp(e, firstName, lastName, email, password)}
+						>
+							Sign Up
+						</button>
+					</>
+				) : (
+					<>
+						<input
+							type="email"
+							placeholder="Email"
+							className="w-full mb-5 mt-5 p-2 text-md outline-none rounded-sm"
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+						/>
+						<input
+							type="password"
+							placeholder="Password"
+							className="w-full p-2 mt-5 text-md outline-none rounded-sm"
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+						/>
+						<button
+							className="bg-green-600 font-semibold text-white text-lg mt-10 py-2 p-2 w-full mb-10 rounded-sm"
+							// onClick={e => signIn(e, firstName, lastName, email, password)}
+						>
+							Sign In
+						</button>
+					</>
+				)}
 			</form>
 			<div className="flex items-center justify-center">
 				<span className="border-t border-gray-300 w-60"></span>
