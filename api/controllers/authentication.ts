b1431c6ea228673ee935c9256d17021983369c5f
Hairum Qureshi <hqureshi@udel.edu>
2024-10-11 20:15:54 -0400

    updated logic to add Google user data to Mongo with custom ID

--- api/controllers/authentication.ts
+++ api/controllers/authentication.ts
@@ -1,15 +1,34 @@
 import { Request, Response } from "express";
+import { nanoid } from "nanoid";
+import User from "../models/user";
 
-const googleAuth = (req: Request, res: Response) => {
+const googleAuth = async (req: Request, res: Response) => {
 	const { email, first_name, last_name, full_name, pfp } = req.body;
 
-	console.log({
-		email,
-		first_name,
-		last_name,
-		full_name,
-		pfp
-	});
+	try {
+		const uuid: string = nanoid().replace(/-/g, "").replace(/_/g, "");
+
+		const user = await User.create({
+			_id: uuid,
+			first_name,
+			last_name,
+			full_name,
+			isGoogleAccount: true,
+			email,
+			pfp
+		});
+
+		console.log(user);
+		res.status(201).json({ message: "User created successfully", user });
+	} catch (error) {
+		console.error(
+			"<authentication.ts> (controllers folder) ERROR".red.bold,
+			(error as Error).toString()
+		);
+		// res
+		// 	.status(500)
+		// 	.json({ message: "Error creating user", error: error.message });
+	}
 };
 
 export { googleAuth };
