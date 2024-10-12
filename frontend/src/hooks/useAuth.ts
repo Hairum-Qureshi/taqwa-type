b96b4b74ea1078b9bdbaffe4e118738d76bbd0cf
Hairum Qureshi <hqureshi@udel.edu>
2024-10-12 00:57:36 -0400

    implemented signup function

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -8,6 +8,13 @@ interface AuthTools {
 		full_name: string,
 		pfp: string
 	) => void;
+	signUp: (
+		e: React.FormEvent,
+		first_name: string,
+		last_name: string,
+		email: string,
+		password: string
+	) => void;
 }
 
 export default function useAuth(): AuthTools {
@@ -18,18 +25,69 @@ export default function useAuth(): AuthTools {
 		full_name: string,
 		pfp: string
 	) {
-		try {
-			axios.post("http://localhost:4000/api/auth/sign-in/google", {
-				email,
-				first_name,
-				last_name,
-				full_name,
-				pfp
+		axios
+			.post(
+				"http://localhost:4000/api/auth/sign-in/google",
+				{
+					email,
+					first_name,
+					last_name,
+					full_name,
+					pfp
+				},
+				{
+					withCredentials: true
+				}
+			)
+			.then(response => {
+				console.log(response.data);
+			})
+			.catch(error => {
+				console.error(error);
 			});
-		} catch (error) {
-			console.log("There was an error", error);
+	}
+
+	function signUp(
+		e: React.FormEvent,
+		first_name: string,
+		last_name: string,
+		email: string,
+		password: string
+	) {
+		e.preventDefault();
+		if (!first_name || !last_name || !email || !password) {
+			alert("Please fill in all fields");
+		} else {
+			const full_name = `${first_name} ${last_name}`;
+
+			if (email.endsWith("@gmail.com")) {
+				alert(
+					"It looks like you're signing up with a Gmail account. Consider signing in with Google!"
+				);
+			} else {
+				axios
+					.post(
+						"http://localhost:4000/api/auth/sign-up",
+						{
+							first_name,
+							last_name,
+							full_name,
+							email,
+							password
+						},
+						{
+							withCredentials: true
+						}
+					)
+					.then(response => {
+						console.log(response.data);
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			}
 		}
 	}
 
-	return { googleAuth };
+	return { googleAuth, signUp };
 }
