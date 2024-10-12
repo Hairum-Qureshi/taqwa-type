5cff2773e206c26496aab1672e7b6d570fdef3e7
Hairum Qureshi <hqureshi@udel.edu>
2024-10-12 00:57:11 -0400

    created helper function to check if the user exists by email and created logic for signing the user up

--- api/controllers/authentication.ts
+++ api/controllers/authentication.ts
@@ -2,33 +2,85 @@ import { Request, Response } from "express";
 import { nanoid } from "nanoid";
 import User from "../models/user";
 
-const googleAuth = async (req: Request, res: Response) => {
-	const { email, first_name, last_name, full_name, pfp } = req.body;
+async function checkIfUserExists(email: string): Promise<boolean> {
+	const user: Document[] = await User.find({ email });
+	return user.length !== 0;
+}
 
+const googleAuth = async (req: Request, res: Response) => {
 	try {
+		// TODO - encrypt password
+		// TODO - check if email is valid
+
+		const { email, first_name, last_name, full_name, pfp } = req.body;
+
 		const uuid: string = nanoid().replace(/-/g, "").replace(/_/g, "");
 
-		const user = await User.create({
-			_id: uuid,
-			first_name,
-			last_name,
-			full_name,
-			isGoogleAccount: true,
-			email,
-			pfp
-		});
+		const userExists: boolean = await checkIfUserExists(email);
 
-		console.log(user);
-		res.status(201).json({ message: "User created successfully", user });
+		if (!userExists) {
+			const createdUser = await User.create({
+				_id: uuid,
+				first_name,
+				last_name,
+				full_name,
+				isGoogleAccount: true,
+				email,
+				pfp
+			});
+
+			res.status(201).send(createdUser);
+			console.log(createdUser);
+		} else {
+			res.status(200).json({ message: "User already exists" });
+		}
 	} catch (error) {
 		console.error(
-			"<authentication.ts> (controllers folder) ERROR".red.bold,
+			"<authentication.ts> (controllers folder) googleAuth function ERROR".red
+				.bold,
 			(error as Error).toString()
 		);
-		// res
-		// 	.status(500)
-		// 	.json({ message: "Error creating user", error: error.message });
+		res.status(500).json({
+			message: (error as Error).toString()
+		});
 	}
 };
 
-export { googleAuth };
+const signUp = async (req: Request, res: Response) => {
+	const { first_name, last_name, full_name, email, password } = req.body;
+
+	try {
+		const uuid: string = nanoid().replace(/-/g, "").replace(/_/g, "");
+		const userExists: boolean = await checkIfUserExists(email);
+		if (!userExists) {
+			const createdUser = await User.create({
+				_id: uuid,
+				first_name,
+				last_name,
+				full_name,
+				isGoogleAccount: false,
+				email,
+				password
+			});
+
+			res.status(201).send(createdUser);
+		} else {
+			const user = await User.find({ email });
+			if (user[0].email.endsWith("@gmail.com")) {
+				res.status(200).json({ message: "Login with Google" });
+			} else {
+				res.status(200).json({ message: "User already exists" });
+			}
+		}
+	} catch (error) {
+		console.error(
+			"<authentication.ts> (controllers folder) signUp function ERROR".red.bold,
+			(error as Error).toString()
+		);
+		res.status(500).json({
+			message: (error as Error).toString()
+		});
+	}
+};
+
+export { googleAuth, signUp };
