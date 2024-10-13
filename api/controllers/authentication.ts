19d76d6748c76a1d446eddeaf2c81314ffb94d30
Hairum Qureshi <hqureshi@udel.edu>
2024-10-13 00:37:33 -0400

    updated sign in and sign up function logic so it encrypts and decrypts passwords as well as checks if the provided password is correct

--- api/controllers/authentication.ts
+++ api/controllers/authentication.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { nanoid } from "nanoid";
 import User from "../models/user";
+import bcrypt from "bcrypt";
 
 async function checkIfUserExists(email: string): Promise<boolean> {
 	const user: Document[] = await User.find({ email });
@@ -28,6 +29,7 @@ const googleAuth = async (req: Request, res: Response) => {
 				email,
 				pfp
 			});
+			// TODO - create a cookie and log the user in via JWT
 
 			res.status(201).send(createdUser);
 			console.log(createdUser);
@@ -53,6 +55,8 @@ const signUp = async (req: Request, res: Response) => {
 		const uuid: string = nanoid().replace(/-/g, "").replace(/_/g, "");
 		const userExists: boolean = await checkIfUserExists(email);
 		if (!userExists) {
+			const hashedPassword = await bcrypt.hash(password, 10);
+
 			const createdUser = await User.create({
 				_id: uuid,
 				first_name,
@@ -60,12 +64,14 @@ const signUp = async (req: Request, res: Response) => {
 				full_name,
 				isGoogleAccount: false,
 				email,
-				password
+				password: hashedPassword
 			});
+
+			// TODO - create a cookie and log the user in via JWT
 
 			res.status(201).send(createdUser);
 		} else {
-			const user = await User.find({ email });
+			const user = await User.find({ email }).lean();
 			if (user[0].email.endsWith("@gmail.com")) {
 				res.status(200).json({ message: "Login with Google" });
 			} else {
@@ -83,4 +89,37 @@ const signUp = async (req: Request, res: Response) => {
 	}
 };
 
-export { googleAuth, signUp };
+const signIn = async (req: Request, res: Response) => {
+	try {
+		const { email, password } = req.body;
+		const userExists: boolean = await checkIfUserExists(email);
+		if (!userExists) {
+			res
+				.status(401)
+				.json({ message: "No account corresponds with this email address" });
+		} else {
+			const user = await User.find({ email }).lean();
+			// const user: Document[] = await User.find({ email });
+			const isPassword: boolean = await bcrypt.compare(
+				password,
+				user[0].password!
+			);
+			if (isPassword) {
+				console.log("Correct password");
+				// TODO - create a cookie and log the user in via JWT
+			} else {
+				res.status(401).json({ message: "Incorrect password" });
+			}
+		}
+	} catch (error) {
+		console.error(
+			"<authentication.ts> (controllers folder) signIn function ERROR".red.bold,
+			(error as Error).toString()
+		);
+		res.status(500).json({
+			message: (error as Error).toString()
+		});
+	}
+};
+
+export { googleAuth, signUp, signIn };
