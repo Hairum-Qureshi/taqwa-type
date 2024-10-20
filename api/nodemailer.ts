4654b3c0b1d4563e90d04d9d7292df79c1617897
Hairum Qureshi <hqureshi@udel.edu>
2024-10-20 01:55:50 -0400

    implemented logic to hide the 'send email' button for the sendReport function if the user has already been warned before

--- api/nodemailer.ts
+++ api/nodemailer.ts
@@ -10,8 +10,8 @@ function callEmailAuth(): nodemailer.Transporter {
 		port: 587,
 		secure: false,
 		auth: {
-			user: process.env.EMAIL, // the email address
-			pass: process.env.APP_PASS // the email address's app password
+			user: process.env.EMAIL,
+			pass: process.env.APP_PASS
 		}
 	});
 
@@ -69,7 +69,6 @@ async function sendReport(
                     <meta charset="UTF-8" />
                     </head>
                     <body>
-                        <div style = "width: 100%; height: 10px; backgroundColor: 'blue';"></div>
                         <h3>Salam Hairum, <br /> ${reporter} reported the following profile for having an inappropriate profile picture:</h3>
                         <a href = ${frontend_base_url}/user/${
 				report._id
@@ -96,9 +95,18 @@ async function sendReport(
                         <button><a href = "${backend_base_url}/api/user/${
 				report._id
 			}/ban" style = "text-decoration: none; color: black;">BAN USER</a></button>
-                        <br />
-                        <p>If you would like the system to send a warning email to the user, press the following button:</p>
-                        <button><a href = "https://www.example.com" style = "text-decoration: none; color: black;">SEND EMAIL</a></button>
+                        <br />                   
+  ${
+		!report.hasBeenWarnedBefore
+			? `<p>If you would like the system to send a warning email to the user, press the following button:</p>
+            <button style="background-color: #4CAF50; padding: 10px 20px; border: none; border-radius: 5px;">
+           <a href="${backend_base_url}/api/user/${report._id}/email" 
+              style="text-decoration: none; color: white;">
+              SEND EMAIL
+           </a>
+         </button>`
+			: `<p>This user has already been warned. You may want to consider banning them.</p>`
+	}
                     </body>
                     </html>`
 		});
@@ -177,7 +185,7 @@ async function sendWarningEmail(email: string, full_name: string) {
                     <meta charset="UTF-8" />
                     </head>
                     <body>
-                        <h3>Salam, ${full_name}, <br /> This is an email to let you know you are receiving a warning for having an inappropriate profile picture. You have two (2) days to change your profile picture. Failure to update it within these two days will result in your account being banned. Taqwa Type is not the kind of site to host inappropriate profile picture that's dedicated to remembering the words of Allah (God). Please understand that inappropriate content of any kind is prohibited. <br /> Thank you. </h3>
+                        <h3>Salam, ${full_name}, <br /> This is an email to let you know you are receiving a warning for having an inappropriate profile picture. You have two (2) days to change your profile picture. Failure to update it within these two days will result in your account being banned. Taqwa Type is not the kind of site to host inappropriate profile pictures as it's a site dedicated to remembering the words of Allah (God) and practicing your typing. Please understand that inappropriate content of any kind is prohibited. <br /> Thank you. </h3>
                     </body>
                     </html>`
 		});
@@ -187,4 +195,36 @@ async function sendWarningEmail(email: string, full_name: string) {
 	}
 }
 
+async function followUpEmail(
+	email: string,
+	full_name: string,
+	pfp: string,
+	hasBeenBannedBefore: boolean
+) {
+	try {
+		const transporter = callEmailAuth();
+		await transporter.sendMail({
+			from: process.env.EMAIL,
+			to: email,
+			subject: "[Taqwa Type] - Warning",
+			html: `<!DOCTYPE html>
+                    <head>
+                    <meta charset="UTF-8" />
+                    </head>
+                    <body>
+                        <h3>Salam Hairum, this is a follow-up email for you to see if ${full_name} has changed their profile picture after you approved to send a warning email to them 2 days ago. If the user's profile picture is still inappropriate, see below for your options. If the user has changed their profile picture to something more appropriate and halal, feel free to discard this email.</h3>
+                        <h4>${
+													hasBeenBannedBefore
+														? "Because this user has been banned before, they will need to be permanently banned."
+														: "Because this user has not been banned before, their account will need to be banned."
+												}</h4>
+                        
+                    </body>
+                    </html>`
+		});
+	} catch (error) {
+		"<nodemailer.ts> sendEmail function error".yellow,
+			(error as Error).toString().red.bold;
+	}
+}
 export { sendReport, sendBanEmail, sendAccountStatusEmail, sendWarningEmail };
