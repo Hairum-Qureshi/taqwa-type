00a1bdb10d75d44b4bfbb8f79f4ae51cc05258e8
Hairum Qureshi <hqureshi@udel.edu>
2024-11-02 20:22:42 -0400

    implemented React Redux Toolkit provider to wrap around App component so state will be accessible throughout the entire application

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { store } from "../app/store.ts";
+import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "../css/index.css";
 
@@ -12,7 +14,9 @@ createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}>
 			<QueryClientProvider client={queryClient}>
-				<App />
+				<Provider store={store}>
+					<App />
+				</Provider>
 				<ReactQueryDevtools />
 			</QueryClientProvider>
 		</GoogleOAuthProvider>
