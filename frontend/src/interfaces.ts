7742a73e280c1f53cf507a8157ec09bbda2c7411
Hairum Qureshi <hqureshi@udel.edu>
2024-11-03 00:39:30 -0400

    created file storing all interfaces

---
+++ frontend/src/interfaces.ts
@@ -0,0 +1,32 @@
+export interface UserData {
+    _id: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    pfp: string,
+    experience: number,
+    totalSurahsCompleted: number,
+    wordsPerMinute: number,
+    accuracy: number,
+    streak: number,
+    createdAt: string
+}
+
+export interface AuthState {
+    user: UserData | null;
+    loading: boolean;
+    error: string | null;
+}
+
+export interface Surah {
+    number: number;
+    name: string;
+    englishName: string;
+    englishNameTranslation: string;
+    numberOfAyahs: number;
+    revelationType: string;
+};
+
+export interface SurahResponse {
+    data: Surah[];
+};
