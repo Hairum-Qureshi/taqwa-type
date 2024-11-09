890c1cc6b1f855a00562c59445a79a26cec5df77
Hairum Qureshi <hqureshi@udel.edu>
2024-11-08 21:29:03 -0500

    added user icon that's only visible to the logged-in user and if clicked, will redirect them to their profile page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,26 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { AppDispatch, RootState } from "../app/store";
+import { useEffect } from "react";
+import { getUserInfo } from "../features/authentication/authSlice";
+import useAccount from "../hooks/useAccount";
 
 export default function Navbar() {
+
+	const { user } = useAccount();
+
 	return (
-		<div className="w-full h-auto text-white bg-slate-800 p-2">
-			<Link to="/">
+		<div className="w-full h-auto text-white bg-slate-800 p-2 justify-center items-center flex">
+			<Link to="/" className = "mr-auto">
 				<h1 className="text-xl">Taqwa Type</h1>
 			</Link>
+			{user && <div className = "text-xl ml-auto mr-5 hover:cursor-pointer">
+				<div className = "border border-white rounded-md p-0.5 w-10 text-center bg-slate-700">
+					<Link to={`/user/${user._id}/account`}><FontAwesomeIcon icon={faUser} /></Link>
+				</div>
+			</div>}
 		</div>
 	);
 }
