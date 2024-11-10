22aae268d84dab6fccb84bf31c289244f8d9f988
Hairum Qureshi <hqureshi@udel.edu>
2024-11-09 21:07:51 -0500

    added conditionals for rendering content on the page such as returning a 404 if the user ID in the URL doesn't exist and adding a loading spinner when loading content

--- frontend/src/components/account-related/Account.tsx
+++ frontend/src/components/account-related/Account.tsx
@@ -1,107 +1,47 @@
 import { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { AppDispatch, RootState } from "../../app/store";
-import { getUserInfo } from "../../features/authentication/authSlice";
-import axios from "axios";
 import { UserData } from "../../interfaces";
 import useAccount from "../../hooks/useAccount";
 import SurahProgressTracker from "./SurahProgressTracker";
+import LoadingSpinner from "../LoadingSpinner";
+import NotFound from "../NotFound";
 // import { logout } from "../features/authentication/authSlice";
 
 export default function Account() {
 	const fileInputRef = useRef<HTMLInputElement>(null);
 	const imageRef = useRef<HTMLImageElement>(null);
 	const [userData, setUserData] = useState<UserData | null>(null);
-	const { user_id } = useParams();
-
-	// const user_data = useSelector((state:RootState) => state.auth.user);
-
-    const { user } = useSelector((state:RootState) => state.auth); 
-	const { getAccountDataByID } = useAccount();
-
-	const dispatch = useDispatch<AppDispatch>();
-
-	useEffect(() => {
-		dispatch(getUserInfo());
-	}, [dispatch]);
-
-
-	useEffect(() => {
-		async function getData() {
-			if(user_id && user_id !== user?._id) {
-				const userData:UserData | null = await getAccountDataByID(user_id);
-				setUserData(userData);
-			}
-			else {
-				setUserData(user);	
-				console.log('x', user);
-			}
-		}
-		getData();
-	}, [user, user_id]);
 
 	// TODO - see about possibly implementing an "estimated reading time/typing time" indicator for each surah also
+	// TODO - add a loading animation prior to when the user views the profile
+	// TODO - need to add a way to prevent users from spanning the "report profile picture" button
+	// TODO - fix minor problem where user isn't getting alerted when they press the "report profile picture" button
 
-	function uploadPfp() {
-		if (fileInputRef.current) {
-			fileInputRef.current.click();
-		}
-	}
+	const { handleImageChange, uploadPfp, reportAccount, user, userData: uData, errorMessage } = useAccount();
 
-	function handleImageChange(event: React.ChangeEvent<HTMLInputElement>) {
-		if (event.target.files && imageRef.current) {
-			const imageFile: File = event.target.files[0];
-			const formData = new FormData();
-			formData.append("profile_picture", imageFile);
-
-			axios
-				.post("http://localhost:4000/api/user/upload/pfp", formData, {
-					withCredentials: true
-				})
-				.then(response => {
-					console.log(response);
-					if (imageRef.current && event.target.files) {
-						imageRef.current.src = window.URL.createObjectURL(
-							event.target.files[0]
-						);
-					}
-				})
-				.catch(error => console.log(error));
-		}
-	}
-
-	function reportAccount() {
-		axios
-			.post("http://localhost:4000/api/user/report", {
-				user_id,
-				withCredentials: true
-			})
-			.then(response => {
-				alert(response.data);
-			})
-			.catch(error => {
-				console.log(error);
-			});
-	}
-
+	useEffect(() => {
+		setUserData(uData);
+	}, [user, uData]);  
+	
 	return (
+		errorMessage !== "" ? <NotFound /> : !user || !userData ? <LoadingSpinner /> : 
 		<div className="h-screen lg:mx-20">
 			<div className="w-full h-full bg-gray-200">
 				<div className="flex-grow bg-slate-300 p-5 h-1/4 flex items-center justify-left">
 					<div
-						className={`border-2 border-black lg:w-40 lg:h-40 w-32 h-32 rounded-md flex-shrink-0 ${user?._id === userData?._id ? "hover:cursor-pointer" : ""}`}
-						onClick={user?._id === userData?._id ? uploadPfp : undefined }
+						className={`border-2 border-black lg:w-40 lg:h-40 w-32 h-32 rounded-md flex-shrink-0 ${user._id === userData._id ? "hover:cursor-pointer" : ""}`}
+						onClick={user._id === userData._id ? () => uploadPfp(fileInputRef) : undefined }
 					>
+						<label htmlFor="file-upload" className = "hidden">File upload</label>
 						<input
 							type="file"
 							ref={fileInputRef}
 							className="hidden"
 							accept="image/png, image/gif, image/jpeg"
-							onChange={user?._id === userData?._id ? handleImageChange : undefined}
-						/>
+							id = "file-upload"
+							onChange={user._id === userData._id ? (event) => handleImageChange(event, imageRef) : undefined}
+							/>
 						<img
-							src={userData?.pfp || "https://pbs.twimg.com/media/FegInEPXkAAS1PE.png"}
+							src={userData.pfp || "https://pbs.twimg.com/media/FegInEPXkAAS1PE.png"}
 							alt="User pfp"
 							referrerPolicy="no-referrer"
 							className="object-cover w-full h-full"
@@ -110,10 +50,10 @@ export default function Account() {
 					</div>
 					<div className="border-2 border-black w-full p-2 ml-5">
 						<div>
-							<h1 className="font-semibold text-xl">{userData?._id === user_id ? `${userData?.first_name} ${userData?.last_name}` : "Loading..."}</h1>
+							<h1 className="font-semibold text-xl">{`${userData.first_name} ${userData.last_name}`}</h1>
 						</div>
 						<div>Test X</div>
-						{user?._id !== userData?._id && 
+						{user._id !== userData._id && 
 						<div>
 							<button onClick={reportAccount} className = "border-2 border-black rounded-md bg-red-600 text-white px-2 py-1">
 								Report Inappropriate Profile Picture
