7cd8020c7a917eaecd1a4b33ac61ec8d4d9f3742
Hairum Qureshi <hqureshi@udel.edu>
2024-11-10 17:12:49 -0500

    moved useEffect logic to custom useSurah hook

--- frontend/src/components/quran-related/Reference.tsx
+++ frontend/src/components/quran-related/Reference.tsx
@@ -1,46 +1,20 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { Verse } from "../../interfaces";
 import LoadingSpinner from "../LoadingSpinner";
+import useSurah from "../../hooks/useSurah";
 
 export default function Reference() {
-    const [englishSurah, setEnglishSurah] = useState<Verse[]>([]);
-    const { surah_no, ayahs } = useParams();
-
-    useEffect(() => {
-        const getSurahInfo = async () => {
-            try {
-                const englishSurahRes = await axios.get(`https://raw.githubusercontent.com/risan/quran-json/refs/heads/main/data/editions/en.json`);
-                
-                if(ayahs) {
-                    const verses:string[] = ayahs?.split("-");
-                    if(verses.length > 1) {
-                        setEnglishSurah(englishSurahRes.data[Number(surah_no)].slice(Number(verses[0]) - 1, Number(verses[1])));
-                    } 
-                    else {
-                        setEnglishSurah([englishSurahRes.data[Number(surah_no)][Number(verses[0]) - 1]]);
-                    }
-                }
-
-            } catch (error) {
-                console.error(error);
-            }
-        };
-    
-        getSurahInfo();
-    }, [surah_no]); 
+    const { englishSurahData } = useSurah();
     
     return (
-        englishSurah.length === 0 ? <LoadingSpinner /> : 
+        englishSurahData.length === 0 ? <LoadingSpinner /> : 
         <div className="w-3/4 my-10 m-auto p-4 border border-slate-300 rounded-md bg-gray-100">
-            {englishSurah.length > 0 ? (
-                englishSurah.length === 1 ? (
+            {englishSurahData.length > 0 ? (
+                englishSurahData.length === 1 ? (
                 <p className="text-lg mb-5">
-                    ({englishSurah[0].verse}) {englishSurah[0].text}.
+                    ({englishSurahData[0].verse}) {englishSurahData[0].text}.
                 </p>
             ) : (
-                    englishSurah.map((verse: Verse, index: number) => (
+                englishSurahData.map((verse: Verse, index: number) => (
                         <p key={index} className="text-lg mb-5">
                             ({verse.verse}) {verse.text}.
                         </p>
