f3c4f620a2aa62acc65e86ad91c43c44f552ce74
Hairum Qureshi <hqureshi@udel.edu>
2024-11-09 21:06:54 -0500

    updated interfaces.ts file by adding a new return type to the useAccount hook's interface

--- frontend/src/interfaces.ts
+++ frontend/src/interfaces.ts
@@ -48,6 +48,7 @@ export interface AccountHandlers {
     reportAccount: () => void;
     user: UserData | null;
     userData: UserData | null;
+    errorMessage: string;
 }
 
 export interface SurahInfo {
