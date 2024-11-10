3eaf9434406ad96bc8e1f3e10dbd096815ed255a
Hairum Qureshi <hqureshi@udel.edu>
2024-11-09 21:11:30 -0500

    implemented conditional logic to render the NotFound component when appropriate and a loading spinner when the data is loading

--- frontend/src/components/SurahInfo.tsx
+++ frontend/src/components/SurahInfo.tsx
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import { SurahInfo as SurahInfoI, Verse } from "../interfaces";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import NotFound from "./NotFound";
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function SurahInfo() {
     const { surah_no } = useParams();
@@ -12,6 +14,7 @@ export default function SurahInfo() {
 
     // TODO - make the verse links in the info section work
     // TODO - see if you can have it so that the div on the right is the same height as the div on the left and that the content in the right div will utilize a scrollbar if the content on the right exceeds the left div's content
+    // TODO - consider seeing if you can add the surah's name too
 
     useEffect(() => {
         const getSurahInfo = async () => {
@@ -33,8 +36,10 @@ export default function SurahInfo() {
     }, [surah_no]);    
 
     return (
+        Number(surah_no) > 114 ? <NotFound /> : !chapterInfo || englishSurah.length === 0 ? <LoadingSpinner /> : 
         <div className = "lg:flex m-5 border border-slate-300 rounded-md">
-            {chapterInfo && <div
+            {chapterInfo && 
+            <div
                 className="prose bg-gray-100 lg:prose-md prose-h2:mb-2 max-w-none lg:w-1/2 p-10" 
                 dangerouslySetInnerHTML={{ __html: chapterInfo?.text }}
             />}
