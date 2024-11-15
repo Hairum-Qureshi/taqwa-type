9ba7a6b8e4620496d4061af7828c9bc5f0b213e6
Hairum Qureshi <hqureshi@udel.edu>
2024-11-15 15:15:35 -0500

    created a verification token model

---
+++ api/models/verification.ts
@@ -0,0 +1,21 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const verificationSchema = new Schema(
+	{
+		verificationCode: {
+            type: String,
+            default: null
+        },
+        expires: {
+            type: Number,
+            default: null
+        },
+        user_id: {
+            type: String,
+            ref: "User"
+        }
+	}
+);
+
+type VerificationCode = InferSchemaType<typeof verificationSchema>;
+export default model<VerificationCode>("Verification", verificationSchema);
