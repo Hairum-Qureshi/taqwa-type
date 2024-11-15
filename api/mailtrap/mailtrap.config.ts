51f0c6def00fe271c3cbee97d4fb7d55bfc45960
Hairum Qureshi <hqureshi@udel.edu>
2024-11-15 16:21:02 -0500

    created a folder containing files related to mailtrap

---
+++ api/mailtrap/mailtrap.config.ts
@@ -0,0 +1,33 @@
+import { MailtrapTransport } from "mailtrap";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+// ! NOTE: because you signed up with a temporary email address, you'll need to check your temporary email inbox for the emails being sent and not your Gmail
+
+dotenv.config();
+
+const TOKEN = process.env.MAILTRAP_TOKEN!;
+
+export const transport = nodemailer.createTransport(
+  MailtrapTransport({
+    token: TOKEN
+  })
+);
+
+export const sender = {
+  address: process.env.MAILTRAP_DOMAIN!,
+  name: "Taqwa Type",
+};
+
+// const recipients = [
+//   "autk3blmkd@zvvzuv.com",
+// ];
+
+// transport
+//   .sendMail({
+//     from: sender,
+//     to: recipients,
+//     subject: "You are awesome!",
+//     text: "Congrats for sending test email with Mailtrap!"
+//   })
+//   .then(console.log, console.error);
