e1eaa9ffd0fc2f6f5b2656d467bb07d4c6fbc660
Hairum Qureshi <hqureshi@udel.edu>
2024-11-15 17:55:29 -0500

    created route for verifying-email

--- api/routes/authentication.ts
+++ api/routes/authentication.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { googleAuth, signUp } from "../controllers/authentication";
+import { googleAuth, signUp, verifyEmail, signIn } from "../controllers/authentication";
 const router = express.Router();
 
 router.post("/sign-in/google", googleAuth);
 router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
+router.post('/verify-email', verifyEmail);
 
 export default router;
