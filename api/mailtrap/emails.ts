1b9242bb2dbf8cd1b0f90abee8533352950f207a
Hairum Qureshi <hqureshi@udel.edu>
2024-11-15 18:33:33 -0500

    updated imports and variables and created sendWelcomeEmail function

--- api/mailtrap/emails.ts
+++ api/mailtrap/emails.ts
@@ -1,4 +1,5 @@
-import { VERIFICATION_EMAIL } from "./email-templates/verification";
+import { VERIFICATION_TEMPLATE } from "./email-templates/verification";
+import { WELCOME_TEMPLATE } from "./email-templates/welcome";
 import { sender, transport } from "./mailtrap.config";
 
 export default async function sendVerificationEmail(user_email:string, verification_code:string) {
@@ -7,8 +8,24 @@ export default async function sendVerificationEmail(user_email:string, verificat
           from: sender,
           to: [user_email], // recipient is an array of user emails
           subject: "Taqwa Type Verification Email",
-          html: VERIFICATION_EMAIL.replace('{VERIFICATION_CODE}', verification_code),
+          html: VERIFICATION_TEMPLATE.replace('{VERIFICATION_CODE}', verification_code),
           category: "Email Verification"
+        });
+       
+        console.log('Email sent successfully!', response);
+    } catch (error) {
+        console.log('<mailtrap.config.ts> ERROR', (error as Error).toString().red.bold);
+    }
+}
+
+export async function sendWelcomeEmail(user_email:string) {
+    try {
+        const response = await transport.sendMail({
+          from: sender,
+          to: [user_email], // recipient is an array of user emails
+          subject: "Taqwa Type Welcome Email",
+          html: WELCOME_TEMPLATE,
+          category: "Welcome Email"
         });
        
         console.log('Email sent successfully!', response);
