f6eb8bfbd5ca96b899d23b768771b64c42d46b1d
Hairum Qureshi <hqureshi@udel.edu>
2024-11-16 14:44:40 -0500

    created a sign out route

--- api/routes/authentication.ts
+++ api/routes/authentication.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { googleAuth, signUp, verifyEmail, signIn, resetPassword } from "../controllers/authentication";
+import { googleAuth, signUp, verifyEmail, signIn, resetPassword, signOut } from "../controllers/authentication";
 const router = express.Router();
 
 router.post("/sign-in/google", googleAuth);
@@ -7,5 +7,6 @@ router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
 router.post('/verify-email', verifyEmail);
 router.post('/forgot-password', resetPassword);
+router.post("/sign-out", signOut);
 
 export default router;
