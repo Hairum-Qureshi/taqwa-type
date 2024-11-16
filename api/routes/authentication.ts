be45dfb506c4571cbf39e58ca2bf8487a743c0b7
Hairum Qureshi <hqureshi@udel.edu>
2024-11-16 14:11:59 -0500

    created forgot-password POST request route

--- api/routes/authentication.ts
+++ api/routes/authentication.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { googleAuth, signUp, verifyEmail, signIn } from "../controllers/authentication";
+import { googleAuth, signUp, verifyEmail, signIn, resetPassword } from "../controllers/authentication";
 const router = express.Router();
 
 router.post("/sign-in/google", googleAuth);
 router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
 router.post('/verify-email', verifyEmail);
+router.post('/forgot-password', resetPassword);
 
 export default router;
