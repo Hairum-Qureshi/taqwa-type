59d3ad74db2f2fbe01dbc9a90c424b0e0aecd0cc
Hairum Qureshi <hqureshi@udel.edu>
2024-11-16 00:36:39 -0500

    moved IUser to interfaces.ts file

--- api/interfaces.ts
+++ api/interfaces.ts
@@ -3,3 +3,21 @@ import { JwtPayload } from "jsonwebtoken";
 export interface UserJWTPayload extends JwtPayload {
     user_id: string | null;
 }
+
+export interface IUser {
+	_id: string;
+	first_name: string;
+	last_name: string;
+	full_name: string;
+	isGoogleAccount: boolean;
+	email: string;
+	pfp: string;
+	password: string;
+	experience: number;
+	mostPracticedSurah: string;
+	totalSurahsCompleted: string;
+	wordsPerMinute: string;
+	accuracy: number;
+	streak: string;
+	createdAt: string;
+}
