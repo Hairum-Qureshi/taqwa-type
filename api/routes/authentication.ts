45315314c413fa1224128ca7d9ffd4358a44adea
Hairum Qureshi <hqureshi@udel.edu>
2024-11-16 20:26:58 -0500

    updated function imports

--- api/routes/authentication.ts
+++ api/routes/authentication.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { googleAuth, signUp, verifyEmail, signIn, resetPassword, signOut } from "../controllers/authentication";
+import { googleAuth, signUp, signIn, signOut } from "../controllers/auth-related/authentication";
+import { verifyEmail, resetPassword, updatePassword } from "../controllers/auth-related/handlers";
 const router = express.Router();
 
 router.post("/sign-in/google", googleAuth);
@@ -8,5 +9,6 @@ router.post("/sign-in", signIn);
 router.post('/verify-email', verifyEmail);
 router.post('/forgot-password', resetPassword);
 router.post("/sign-out", signOut);
+router.post("/reset-password/:token", updatePassword);
 
 export default router;
