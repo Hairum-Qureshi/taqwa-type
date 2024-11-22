9a5a25518fd006467ea0eee77eea13fd84437f8f
Hairum Qureshi <hqureshi@udel.edu>
2024-11-22 02:06:24 -0500

    modified upload pfp POST request so it first checks if the uploaded image is NSFW or not; if it is, it doesn't save the image and let's the user know, otherwise it updates it

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -17,6 +17,7 @@ import cloundinary_config from "./config/cloudinary";
 import User from "../models/user";
 import { UserJWTPayload } from "../interfaces";
 import { checkAuthStatus } from "../middleware/checkAuthStatus";
+import isNSFW from "../utils/content-moderator";
 
 const router = express.Router();
 
@@ -24,56 +25,65 @@ colors.enable();
 router.get("/:user_id/progress", getUserProgress);
 
 router.post(
-	"/upload/pfp",
-	checkAuthStatus,
-	upload.single("profile_picture"),
-	async (req, res) => {
-		const FOLDER_PATH = path.join(__dirname, "..", "/temp_images");
-		// TODO - need to have middleware check if the user has a cookie or not
-		try {
-			cloundinary_config; // need this here for the config to be recognized
-			const user_cookie: string | undefined = req.cookies["auth-session"];
-			if (user_cookie) {
-				const decoded_cookie: UserJWTPayload = jwtDecode(user_cookie);
-				fs.readdir(FOLDER_PATH, (err, files) => {
-					files.forEach(async file => {
-						const uploadedImagePath = path.resolve(
-							__dirname,
-							`../temp_images/${file}`
-						);
+    "/upload/pfp",
+    checkAuthStatus,
+    upload.single("profile_picture"),
+    async (req, res) => {
+        const FOLDER_PATH = path.join(__dirname, "..", "/temp_images");
+        try {
+            cloundinary_config; // Ensure Cloudinary configuration is loaded
 
-						if (err) {
-							console.error('<user.ts> POST route error', (err as Error).toString().red.bold);
-						} else {
-							const uid = decoded_cookie.user_id;
-							cloudinary.uploader
-								.upload(uploadedImagePath, {
-									public_id: `${uid}-profile_picture` 
-								})
-								.then(async uploadResult => {
-									if (uploadResult?.url) {
-										fs.unlink(path.join(FOLDER_PATH, file), err => {
-											if (err) throw err;
-										});
+            // Read files in the temp_images folder
+            const files = await fs.promises.readdir(FOLDER_PATH);
 
-										await User.findByIdAndUpdate({_id: uid}, {
-											pfp: uploadResult.url || "https://pbs.twimg.com/media/FegInEPXkAAS1PE.png"
-										});
-									}
-								})
-								.catch(error => {
-									console.log('<user.ts> routes folder POST request ERROR'.red.bold, error);
-								});
-						}
-					});
-				});
-      
-				res.status(200).send("Success");
-			}
-		} catch (error) {
-			console.log("There was an error", (error as Error).toString().red.bold);
-		}
-	}
+            for (const file of files) {
+                const uploadedImagePath = path.resolve(__dirname, `../temp_images/${file}`);
+
+                // Check for NSFW content
+                if (await isNSFW(uploadedImagePath)) {
+                    // Remove the file
+                    await fs.promises.unlink(uploadedImagePath);
+
+                    // Respond immediately with an error
+                    res.status(400).json({ message: "NSFW content is not allowed" });
+					return;
+                }
+
+                // Process the file
+                const uid = req.cookies.decoded_uid;
+                try {
+                    const uploadResult = await cloudinary.uploader.upload(uploadedImagePath, {
+                        public_id: `${uid}-profile_picture`,
+                    });
+
+                    if (uploadResult?.url) {
+                        // Delete the local file
+                        await fs.promises.unlink(uploadedImagePath);
+
+                        // Update the user's profile picture in the database
+                        await User.findByIdAndUpdate(
+                            { _id: uid },
+                            {
+                                pfp: uploadResult.url || "https://pbs.twimg.com/media/FegInEPXkAAS1PE.png",
+                            }
+                        );
+                    }
+                } catch (uploadError) {
+                    console.error(
+                        "<user.ts> Cloudinary upload error:".red.bold,
+                        uploadError
+                    );
+                }
+            }
+
+            res.status(200).send("Success");
+			return;
+        } catch (error) {
+            console.error("There was an error", (error as Error).toString().red.bold);
+            res.status(500).send("An error occurred while processing your request.");
+			return;
+        }
+    }
 );
 
 router.post("/report", checkAuthStatus, reportUser);
