fcc6e7884293f2aed796e48dc11203ca6fea6be5
Hairum Qureshi <hqureshi@udel.edu>
2024-11-22 13:18:17 -0500

    imported Fingerprint.js provider

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "../app/store.ts";
 import { Provider } from "react-redux";
 import App from "./App.tsx";
 import "../css/index.css";
-
+import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react'
+  
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
@@ -15,9 +15,10 @@ createRoot(document.getElementById("root")!).render(
 		<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}>
 			<QueryClientProvider client={queryClient}>
 				<Provider store={store}>
-					<App />
+					<FpjsProvider loadOptions={{ apiKey: import.meta.env.FINGERPRINT_JS_API_KEY }}>
+						<App />
+					</FpjsProvider>
 				</Provider>
-				<ReactQueryDevtools />
 			</QueryClientProvider>
 		</GoogleOAuthProvider>
 	</StrictMode>
