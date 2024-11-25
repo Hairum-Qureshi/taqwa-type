9f7044cc6e11f128a5991fec7128e10d5165f5a8
Hairum Qureshi <hqureshi@udel.edu>
2024-11-25 17:50:34 -0500

    added background, hover, and active colors to the user info divs

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -1,5 +1,8 @@
 import { faFlag } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+// TODO - make sure to utilize e.preventPropagation for the flag button
 
 export default function Users() {
   return (
@@ -23,24 +26,47 @@ export default function Users() {
                 </div>
             </div>
         </div>
-        <div className="border-2 border-black mt-5 rounded-md p-1 flex items-center relative">
-            <div className="w-20 h-20 rounded-md flex-shrink-0">
-                <img 
-                    src="https://i.pinimg.com/originals/2c/47/d5/2c47d5dd5b532f83bb55c4cd6f5bd1ef.jpg" 
-                    alt="User profile picture" 
-                    className="w-full h-full rounded-md border-2 border-black object-cover" 
-                />
+        <Link to = "/user/123/account">
+            <div className="border-2 border-black mt-5 rounded-md p-1 flex items-center relative bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
+                <div className="w-20 h-20 rounded-md flex-shrink-0">
+                    <img 
+                        src="https://i.pinimg.com/originals/2c/47/d5/2c47d5dd5b532f83bb55c4cd6f5bd1ef.jpg" 
+                        alt="User profile picture" 
+                        className="w-full h-full rounded-md border-2 border-black object-cover" 
+                    />
+                </div>
+                <div className="w-full ml-2">
+                    <p className="text-sm"><b>John Doe</b></p>
+                    <p className="text-sm">Surahs Practiced: 10/114</p>
+                    <p className="text-sm">Average WPM: 10</p>
+                    <p className="text-sm">Overall Accuracy: 10%</p>
+                </div>
+                <div className = "w-8 h-8 flex justify-center items-center absolute top-0 right-0 bg-red-600 rounded-tr-sm text-white active:bg-red-700 hover:cursor-pointer" title = "Report this profile">
+                    <FontAwesomeIcon icon={faFlag} />
+                </div>
             </div>
-            <div className="w-full ml-2">
-                <p className="text-sm"><b>John Doe</b></p>
-                <p className="text-sm">Surahs Practiced: 10/114</p>
-                <p className="text-sm">Average WPM: 10</p>
-                <p className="text-sm">Overall Accuracy: 10%</p>
+        </Link>
+        <Link to = "/user/123/account">
+            <div className="border-2 border-black mt-5 rounded-md p-1 flex items-center relative bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
+                <div className="w-20 h-20 rounded-md flex-shrink-0">
+                    <img 
+                        src="https://i.pinimg.com/originals/2c/47/d5/2c47d5dd5b532f83bb55c4cd6f5bd1ef.jpg" 
+                        alt="User profile picture" 
+                        className="w-full h-full rounded-md border-2 border-black object-cover" 
+                    />
+                </div>
+                <div className="w-full ml-2">
+                    <p className="text-sm"><b>Jane Doe</b></p>
+                    <p className="text-sm">Surahs Practiced: 5/114</p>
+                    <p className="text-sm">Average WPM: 55</p>
+                    <p className="text-sm">Overall Accuracy: 50%</p>
+                </div>
+                <div className = "w-8 h-8 flex justify-center items-center absolute top-0 right-0 bg-red-600 rounded-tr-sm text-white active:bg-red-700 hover:cursor-pointer" title = "Report this profile">
+                    <FontAwesomeIcon icon={faFlag} />
+                </div>
             </div>
-            <div className = "w-8 h-8 flex justify-center items-center absolute top-0 right-0 bg-red-600 rounded-tr-sm text-white active:bg-red-700 hover:cursor-pointer" title = "Report this profile">
-                <FontAwesomeIcon icon={faFlag} />
-            </div>
-        </div>
+        </Link>
     </div>
   )
 }
+
