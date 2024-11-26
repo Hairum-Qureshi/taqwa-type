ed71157f639946e90c8fe76a2f577fe6b8ad7ca0
Hairum Qureshi <hqureshi@udel.edu>
2024-11-26 01:00:12 -0500

    removed unnecessary 'ear' accidentally typed

--- frontend/src/components/user-related/Users.tsx
+++ frontend/src/components/user-related/Users.tsx
@@ -15,7 +15,7 @@ export default function Users() {
         <div className = "lg:w-1/2 w-full m-auto mt-5 p-3">
         <h1 className = "text-2xl font-semibold mb-3">All Users</h1>
         <input type="text" placeholder = "Search user by name" className = "border border-black rounded-md w-full p-1 outline-none px-3" />
-        <div className = "mt-2">ear
+        <div className = "mt-2">
             <h3>Sort By:</h3>
             <div className="flex text-sm">
                 <div className="p-1 border border-black mr-2 rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
