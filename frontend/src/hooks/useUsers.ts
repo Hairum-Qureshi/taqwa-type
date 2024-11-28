1384077cd35794af258d27084663b67f9891b86e
Hairum Qureshi <hqureshi@udel.edu>
2024-11-28 00:18:00 -0500

    added code for updating loading status state and exporting it

--- frontend/src/hooks/useUsers.ts
+++ frontend/src/hooks/useUsers.ts
@@ -10,8 +10,10 @@ export default function useUsers():UserHandlers {
     const [maxPages, setMaxPages] = useState(1);
     const [numUsers, setNumUsers] = useState(0);
     const [filterType, setFilterType] = useState(searchParams.get("filter") || "default");
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         async function getAllUsers() {
             axios.get(`http://localhost:4000/api/user/all-users?page=${queryPage}&filter=${filterType}`, {
                 withCredentials: true
@@ -19,13 +21,17 @@ export default function useUsers():UserHandlers {
                 setAllUserData(response.data.users);
                 setMaxPages(response.data.pageCount);
                 setNumUsers(response.data.numUsers);
+                setLoading(false);
             }).catch(error => {
                 console.log(error);
+                setLoading(false);
             });
         }
     
         getAllUsers();
     }, []);
+
+    // TODO - need to detect when the /users route has no search params (i.e. when you click on the link to the users page after having a search param in the URL)
 
     function handleNextPage() {
         const page = (Number(searchParams.get("page")) || 1) + 1;
@@ -38,6 +44,8 @@ export default function useUsers():UserHandlers {
     }
     
     function filterWPM() {
+        setLoading(true);
+
         if (numUsers < 10) {
             window.location.href = `/users?filter=wpm`;
         }
@@ -49,6 +57,8 @@ export default function useUsers():UserHandlers {
     }
 
     function filterAccuracy() {
+        setLoading(true);
+
         if (numUsers < 10) {
             window.location.href = `/users?filter=accuracy`;
         }
@@ -60,6 +70,8 @@ export default function useUsers():UserHandlers {
     }
 
     function filterSurahsPracticed() {
+        setLoading(true);
+
         if (numUsers < 10) {
             window.location.href = `/users?filter=surahs-practiced`;
         }
@@ -71,6 +83,8 @@ export default function useUsers():UserHandlers {
     }
 
     function filterDateJoined() {
+        setLoading(true);
+
         if (numUsers < 10) {
             window.location.href = `/users?filter=date-joined`;
         }
@@ -80,6 +94,7 @@ export default function useUsers():UserHandlers {
 
         setFilterType("date-joined");
     }
+    
 
-    return { allUserData, handleNextPage, handlePreviousPage, queryPage, maxPages, numUsers, filterWPM, filterAccuracy, filterSurahsPracticed, filterDateJoined };
+    return { allUserData, handleNextPage, handlePreviousPage, queryPage, maxPages, numUsers, filterWPM, filterAccuracy, filterSurahsPracticed, filterDateJoined, loading };
 }
