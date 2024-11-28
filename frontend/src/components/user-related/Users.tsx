b9bc2595089beae172005145edacdaf8b5d4969a
Hairum Qureshi <hqureshi@udel.edu>
2024-11-27 22:11:39 -0500

    styling for keeping the buttons at the bottom and disabling prev button if at the first page and next button if at the last page and added styling to the buttons

--- frontend/src/components/user-related/Users.tsx
+++ frontend/src/components/user-related/Users.tsx
@@ -7,35 +7,42 @@ import UserInfoContainer from "./UserInfoContainer";
 // TODO - make sure to utilize e.stopPropagation for the flag button
 
 export default function Users() {
-    const { allUserData } = useUsers();
+    const { allUserData, handleNextPage, handlePreviousPage, queryPage } = useUsers();
     const { user:currentUserData } = useAccount();
+    const pageCount = 10;
 
     return (
         !currentUserData ? <NotFound /> : 
-        <div className = "lg:w-1/2 w-full m-auto mt-5 p-3">
-        <h1 className = "text-2xl font-semibold mb-3">All Users</h1>
-        <input type="text" placeholder = "Search user by name" className = "border border-black rounded-md w-full p-1 outline-none px-3" />
-        <div className = "mt-2">
-            <h3>Sort By:</h3>
-            <div className="flex text-sm">
-                <div className="p-1 border border-black mr-2 rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
-                    Words/Min (WPM)
-                </div>
-                <div className="p-1 border border-black mr-2 rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
-                    Accuracy
-                </div>
-                <div className="p-1 border border-black mr-2 rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
-                    Surahs Practiced
-                </div>
-                <div className="p-1 border border-black rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
-                    Date Joined
+        <div className = "lg:w-1/2 w-full m-auto mt-5 p-3 h-screen relative">
+            <h1 className = "text-2xl font-semibold mb-3">All Users</h1>
+            <input type="text" placeholder = "Search user by name" className = "border border-black rounded-md w-full p-1 outline-none px-3" />
+            <div className = "mt-2">
+                <h3>Sort By:</h3>
+                <div className="flex text-sm">
+                    <div className="p-1 border border-black mr-2 rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
+                        Words/Min (WPM)
+                    </div>
+                    <div className="p-1 border border-black mr-2 rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
+                        Accuracy
+                    </div>
+                    <div className="p-1 border border-black mr-2 rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
+                        Surahs Practiced
+                    </div>
+                    <div className="p-1 border border-black rounded-md flex-1 text-center hover:cursor-pointer bg-gray-100 hover:bg-gray-200 active:bg-gray-300">
+                        Date Joined
+                    </div>
                 </div>
             </div>
+            {allUserData?.map((user:UserData) => {
+                return <UserInfoContainer user_id={user._id} full_name = {`${user.first_name} ${user.last_name}`} wpm = {user.wordsPerMinute} surahsPracticed = {user.totalSurahsCompleted} accuracy = {user.accuracy} pfp = {user.pfp} />
+            })}
+            <div className="w-full my-20 absolute bottom-0 flex justify-around">
+            <div className="flex space-x-4">
+                <button className="border border-slate-500 rounded-md p-1 w-32 disabled:bg-slate-800 bg-slate-400 disabled:hover:cursor-not-allowed text-white" onClick = {handlePreviousPage} disabled = {queryPage === 1}>Previous</button>
+                <button className="border border-slate-500 rounded-md p-1 w-32 disabled:bg-slate-800 bg-slate-400 disabled:hover:cursor-not-allowed text-white" onClick = {handleNextPage} disabled = {queryPage === pageCount}>Next</button>
+            </div>
+            </div>
         </div>
-        {allUserData?.map((user:UserData) => {
-            return <UserInfoContainer user_id={user._id} full_name = {`${user.first_name} ${user.last_name}`} wpm = {user.wordsPerMinute} surahsPracticed = {user.totalSurahsCompleted} accuracy = {user.accuracy} pfp = {user.pfp} />
-        })}
-    </div>
     )
 }
 
