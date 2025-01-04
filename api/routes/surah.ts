cc4ff9db137bc94765bfdd0f6ca13919e4a60adc
Hairum Qureshi <hqureshi@udel.edu>
2025-01-03 19:20:26 -0500

    created POST request for saving the user progress responsible for surahs with sections

--- api/routes/surah.ts
+++ api/routes/surah.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import colors from "colors";
 import { checkAuthStatus } from "../middleware/checkAuthStatus";
-import { saveProgress } from "../controllers/surah";
+import { saveProgress, saveSectionProgress } from "../controllers/surah";
 
 const router = express.Router();
 
 colors.enable();
 
 router.post('/save-progress', checkAuthStatus, saveProgress);
+router.post('/save-progress/section/:section_no', checkAuthStatus, saveSectionProgress);
 
 export default router;
